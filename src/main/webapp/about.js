aac721a23a431bccd437ef23042a113bdb1c5bf8
Abrham Kifle Kebede <abrihott@gmail.com>
2025-01-04 09:20:53 +0300

    Update about.js

--- src/main/webapp/about.js
+++ src/main/webapp/about.js
@@ -1,65 +1,12 @@
-const items = document.querySelectorAll('.Contriutor');
-    let currentIndex = Math.floor(items.length / 2);
-
-    function showContributor() {
-        items.forEach((Contriutor, index) => {
-            Contriutor.classList.remove('active');
-            Contriutor.classList.remove('blur');
-            if (index === currentIndex) {
-                Contriutor.classList.add('active');
-            } else {
-                Contriutor.classList.add('blur');
-            }
-        });
-    }
-    document.querySelector('.prev').addEventListener('click', () => {
-        currentIndex = (currentIndex - 1 + items.length) % items.length;
-        showContributor();
-    });
-
-    document.querySelector('.next').addEventListener('click', () => {
-        currentIndex = (currentIndex + 1) % items.length;
-        showContributor();
-    });
-const progress = document.querySelectorAll('.Progress');
-let current = Math.floor(progress.length / 2);
-
-
-    function showProgress() {
-        progress.forEach((Progress, index) => {
-            Progress.classList.remove('active');
-            Progress.classList.remove('hidde');
-            if (index === current) {
-                Progress.classList.add('active');
-            } else {
-                Progress.classList.add('hidde');
-            }
-        });
-    }
-    document.querySelector('.prev').addEventListener('click', () => {
-        current = (current - 1 + progress.length) % progress.length;
-        showProgress();
-    });
-
-    document.querySelector('.next').addEventListener('click', () => {
-        current = (current + 1) % progress.length;
-        showProgress();
-    });
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
 
 next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.Contriutor')
+    let items = document.querySelectorAll('.item')
     document.querySelector('.slide').appendChild(items[0])
-    let peogress = document.querySelectorAll('.Progress')
-    document.querySelector('.Contributions').appendChild(peogress[0])
 })
 
 prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.Contriutor')
+    let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
-    let progress = document.querySelectorAll('.Progress')
-    document.querySelector('.Contributions').prepend(progress[progress.length - 1])
 })
-    showContributor();
-    showProgress();
